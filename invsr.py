e89f6354415f21365878444b4d3982fa38055662
Map9876 <hhdheuwjbgg@outlook.com>
2025-01-09 17:07:19 +0800

    Update invsr.py

--- invsr.py
+++ invsr.py
@@ -8,9 +8,10 @@ import subprocess
 lines = [
     "mkdir InvSR",
     "git clone https://github.com/zsyOAOA/InvSR.git",
+    "cd InvSR",    
     "pip install torch==2.4.0 torchvision==0.19.0 torchaudio==2.4.0 --index-url https://download.pytorch.org/whl/cu121",  
     "pip install -U xformers==0.0.27.post2  --index-url https://download.pytorch.org/whl/cu121",  
-    "pip install -e \".[torch]\"", 
+    "pip install -e '.[torch]'", 
     "pip install -r requirements.txt",  
 ]
 
